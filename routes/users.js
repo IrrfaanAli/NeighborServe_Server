10bb1259c406dfc394651b8d04b4b3a92d389da5
Irfan Ali <alimohammadirfan7@gmail.com>
2023-11-02 20:01:17 +0600

    update done

--- routes/users.js
+++ routes/users.js
@@ -1,126 +1,173 @@
-const express = require("express");
-const router = express.Router();
-const client = require("../database/db");
-const { ObjectId } = require("mongodb");
-const bcrypt = require("bcrypt");
+const express = require("express")
+const router = express.Router()
+const client = require("../database/db")
+const { ObjectId } = require("mongodb")
+const bcrypt = require("bcrypt")
 
-const usersCollection = client.db("NeighborServe").collection("UsersData");
+const usersCollection = client.db("NeighborServe").collection("UsersData")
 
 router.get("/", async (req, res) => {
-  const result = await usersCollection.find().limit(5).toArray();
-  res.send(result);
-});
+  const result = await usersCollection.find().limit(5).toArray()
+  res.send(result)
+})
 
 router.get("/provider", async (req, res) => {
-  const query = { user_type: "Service Provider" };
+  const query = { user_type: "Service Provider" }
 
-  const result = await usersCollection.find(query).limit(8).toArray();
-  res.send(result);
-});
+  const result = await usersCollection.find(query).limit(8).toArray()
+  res.send(result)
+})
 
 router.get("/provider/:type", async (req, res) => {
-  const type = req.params.type;
+  const type = req.params.type
 
-  const query = { user_category: type };
+  const query = { user_category: type }
 
-  const result = await usersCollection.find(query).limit(12).toArray();
-  res.send(result);
-});
+  const result = await usersCollection.find(query).limit(12).toArray()
+  res.send(result)
+})
 router.get("/provider/details/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await usersCollection.findOne(query);
-  res.send(result);
-});
+  const id = req.params.id
+  const query = { _id: new ObjectId(id) }
+  const result = await usersCollection.findOne(query)
+  res.send(result)
+})
 
 router.post("/signup", async (req, res) => {
-  const { name, email, role, password, phone } = req.body;
+  const {
+    user_fullname,
+    user_email,
+    user_pass,
+    user_dob,
+    user_gender,
+    user_type,
+    user_status,
+    user_location,
+    user_lat,
+    user_lon,
+    user_phone,
+    user_regYear,
+  } = req.body
 
-  const query = { email };
-  const existingUser = await usersCollection.findOne(query);
+  const query = { user_email }
+  const existingUser = await usersCollection.findOne(query)
   if (existingUser) {
-    return res.send({ message: "user already exists" });
+    return res.send({ message: "user already exists" })
   }
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(user_pass, 10)
   const newUser = {
-    name,
-    email,
-    role,
-    password: hashedPassword,
-    phone,
-  };
-  const result = await usersCollection.insertOne(newUser);
-  res.send(result);
-});
+    user_fullname,
+    user_email,
+    user_pass: hashedPassword,
+    user_dob,
+    user_gender,
+    user_type,
+    user_status,
+    user_location,
+    user_lat,
+    user_lon,
+    user_phone,
+    user_regYear,
+  }
+  const result = await usersCollection.insertOne(newUser)
+  res.send(result)
+})
 router.post("/providersignup", async (req, res) => {
-  const { name, email, role, password, phone, location, category } = req.body;
-  console.log(req.body);
+  const { user_fullname,
+    user_email,
+    user_pass,
+    user_dob,
+    user_gender,
+    user_type,
+    user_category,
+    user_status,
+    user_location,
+    user_lat,
+    user_lon,
+    user_phone,
+    user_regYear,
+    user_hireCount,
+    user_verficationStatus,
+    user_serviceDetails,
+    user_respondTime
+  } = req.body
+  console.log(req.body)
 
-  const query = { email };
-  const existingUser = await usersCollection.findOne(query);
+  const query = { user_email }
+  const existingUser = await usersCollection.findOne(query)
   if (existingUser) {
-    return res.send({ message: "user already exists" });
+    return res.send({ message: "user already exists" })
   }
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(user_pass, 10)
   const newUser = {
-    name,
-    email,
-    role,
-    password: hashedPassword,
-    phone,
-    location,
-    category,
-  };
-  const result = await usersCollection.insertOne(newUser);
-  res.send(result);
-});
+    user_fullname,
+    user_email,
+    user_pass: hashedPassword,
+    user_dob,
+    user_gender,
+    user_type,
+    user_category,
+    user_status,
+    user_location,
+    user_lat,
+    user_lon,
+    user_phone,
+    user_regYear,
+    user_hireCount,
+    user_verficationStatus,
+    user_serviceDetails,
+    user_respondTime
+  }
+  const result = await usersCollection.insertOne(newUser)
+  res.send(result)
+})
 
 router.post("/users/googlelogin", async (req, res) => {
-  const user = req.body;
+  const user = req.body
 
-  const query = { email: user.email };
-  const existingUser = await usersCollection.findOne(query);
+  const query = { email: user.email }
+  const existingUser = await usersCollection.findOne(query)
 
   if (existingUser) {
-    return res.send({ message: "user already exists" });
+    return res.send({ message: "user already exists" })
   }
 
-  const result = await usersCollection.insertOne(user);
-  res.send(result);
-});
+  const result = await usersCollection.insertOne(user)
+  res.send(result)
+})
 
 router.get("/admin/:email", async (req, res) => {
-  const email = req.params.email;
+  const email = req.params.email
 
-  const query = { email: email };
+  const query = { email: email }
 
-  const user = await usersCollection.findOne(query);
+  const user = await usersCollection.findOne(query)
 
-  const result = { admin: user?.role === "admin" };
+  const result = { admin: user?.role === "admin" }
 
-  res.send(result);
-});
+  res.send(result)
+})
 router.get("/provider/:email", async (req, res) => {
-  const email = req.params.email;
+  const email = req.params.email
 
-  const query = { email: email };
+  const query = { email: email }
 
-  const user = await usersCollection.findOne(query);
+  const user = await usersCollection.findOne(query)
 
-  const result = { provider: user?.role === "provider" };
+  const result = { provider: user?.role === "provider" }
 
-  res.send(result);
-});
+  res.send(result)
+})
 router.get("/user/:email", async (req, res) => {
-  const email = req.params.email;
+  const email = req.params.email
 
-  const query = { email: email };
+  const query = { email: email }
 
-  const user = await usersCollection.findOne(query);
+  const user = await usersCollection.findOne(query)
 
-  const result = { User: user?.role === "user" };
+  const result = { User: user?.role === "user" }
 
-  res.send(result);
-});
+  res.send(result)
+})
 
-module.exports = router;
+module.exports = router
